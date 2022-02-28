1bfecbf5bc9a6b3578669de1a1b71919d9c561d0
Stephen Lockerbie <stephen.j.lockerbie@gmail.com>
2022-02-28 15:33:23 +0200

    Menu and resources

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from art import logo
+from menu import MENU as menu
+
+# For the resources assume that the machine has:
+# 3 litres of water
+# 2 litres of milk substitute
+# 1 kilogram of coffee
+
+resources = {
+    "water": 3000,
+    "milk": 2000,
+    "coffee": 1000,
+}
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def boot_menu():
+    pass
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    boot_menu()
